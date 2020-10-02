5e6b1177e9e22f12660fbca47c32dba221a54dbf
tahmid17 <60854686+tahmid17@users.noreply.github.com>
2020-10-02 19:49:41 +0600

    Create InsertionSort.cpp

---
+++ InsertionSort.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void insertionSort(int a[],int n){
+int i,j,item;
+
+for(i=1;i<n;i++){
+    item=a[i];
+
+    j=i-1;
+    while(j>=0&&a[j]>item){
+        a[j+1]=a[j];
+        j--;
+    }
+a[j+1]=item;
+}
+
+}
+
+
+int main(){
+int n;
+cin>>n;
+int array1[n];
+for(int i=0;i<n;i++)
+    cin>>array1[i];
+insertionSort(array1,n);
+for(int i=0;i<n;i++)
+    cout<<array1[i]<<" ";
+}
