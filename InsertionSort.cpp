31cb36dd3305a82d9e9aa2c4e62015ea96bf9745
tahmid17 <60854686+tahmid17@users.noreply.github.com>
2020-10-02 19:50:10 +0600

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,29 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-void insertionSort(int a[],int n){
-int i,j,item;
+void insertionSort(int a[],int n)
+{
+    int i,j,item;
 
-for(i=1;i<n;i++){
-    item=a[i];
+    for(i=1; i<n; i++)
+    {
+        item=a[i];
 
-    j=i-1;
-    while(j>=0&&a[j]>item){
-        a[j+1]=a[j];
-        j--;
+        j=i-1;
+        while(j>=0&&a[j]>item)
+        {
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=item;
     }
-a[j+1]=item;
-}
 
 }
 
 
-int main(){
-int n;
-cin>>n;
-int array1[n];
-for(int i=0;i<n;i++)
-    cin>>array1[i];
-insertionSort(array1,n);
-for(int i=0;i<n;i++)
-    cout<<array1[i]<<" ";
+int main()
+{
+    int n;
+    cin>>n;
+    int array1[n];
+    for(int i=0; i<n; i++)
+        cin>>array1[i];
+    insertionSort(array1,n);
+    for(int i=0; i<n; i++)
+        cout<<array1[i]<<" ";
 }
